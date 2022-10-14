ee2975cc9a8c5d967fdf5c9488c9056a6b004933
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-10-14 12:35:06 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,3 +5,5 @@ int main() {
   printf("%d\n",one(1));
   return 1 - 1;
 }
+
+/* One is my favorite number, that and 100000001. 100000001 is evenly divisible by 17 (which is weird). If you don't beleive me, look it up. /*
